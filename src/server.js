6fe4441e4da3fe00914449cc2f508e105aa587f0
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-05 10:16:28 +0300

    express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,30 @@
-console.log("Hello world");
+// console.log("Hello world");
+
+import { default as express } from "express"; // require eiluite pakeista i import 
+
+const app = express(); // paleidziama funkcija is node_modules
+const port = 3000;
+
+
+// atejus uzklausai get i / suveiks sita funkcija
+app.get('/', (req, res) => {
+    res.send('Hello World!') // israsoma narsykleje 
+});
+
+// atejus uzklausai get i /labas suveiks sita funkcija
+app.get('/labas', (req, res) => {
+    res.send('Labas pasauli') // israsoma narsykleje 
+});
+
+// atejus uzklausai get i /labas/siandien suveiks sita funkcija
+app.get('/labas/siandien', (req, res) => {
+    res.send('Labas pasauli, siandien ketvirtadienis!') // israsoma narsykleje 
+});
+
+// klauso 3000 port'o 
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
+
+
+// narsykle paleidziant rasoma localhost:3000/ ir yra atvaizduojama, kas yra siunciama is serverio
